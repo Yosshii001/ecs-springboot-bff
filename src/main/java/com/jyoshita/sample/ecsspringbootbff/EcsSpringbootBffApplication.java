8ebe74655547291df7993b5ac961d0785c86ad03
yoshitanijunichi <jun.and.guri@gmail.com>
2020-12-30 00:00:57 +0900

    initial commit

---
+++ src/main/java/com/jyoshita/sample/ecsspringbootbff/EcsSpringbootBffApplication.java
@@ -0,0 +1,13 @@
+package com.jyoshita.sample.ecsspringbootbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsSpringbootBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsSpringbootBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyoshita/sample/ecsspringbootbff/EcsSpringbootBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyoshita.sample.ecsspringbootbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsSpringbootBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
